28361151ba200e3e1976ed56ad55596794b6d8f8
Nick Guletskii <nick@nickguletskii.com>
2019-12-30 16:37:48 +0300

    Set up automatic SCM-based versioning

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='grundzeug',
-    version='0.1.0',
-    packages=['grundzeug', 'grundzeug.util', 'grundzeug.config', 'grundzeug.container', 'grundzeug.container.common',
-              'grundzeug.container.di',
-              'grundzeug.container.plugins'],
-    url='',
-    license='',
-    author='Nick Guletskii',
-    author_email='nick@nickguletskiki.com',
-    description=''
+    name="grundzeug",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    url="",
+    license="Apache License 2.0",
+    author="Nick Guletskii",
+    author_email="nick@nickguletskii.com",
+    description="Grundzeug Dependency Injection container and configuration management framework",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
 )
